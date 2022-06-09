4cd09d693e7964bbf6e57d127822e57a2e1af78f
M.Sarathi Ganesh <100061035+Noah-Cooper47@users.noreply.github.com>
2022-06-09 21:22:39 +0530

    Add files via upload

---
+++ program.java
@@ -0,0 +1,28 @@
+package Day1;
+
+public class program {
+
+	public static void main(String[] args) {
+System.out.println("Hello world!");
+int x = 5;
+int y = 7;
+int z = 57;
+int sum = x + y + z;
+int min = x- y - z;
+int multi = x*y*z;
+double div = x / (double) y;
+double div2 = z/ (double)y;
+double expo = Math.pow(x,y) ;
+System.out.println(sum);
+System.out.println(min);
+System.out.println(multi);
+System.out.println(div);
+System.out.println(div2);
+System.out.println(expo);
+String fir = "dARK_Wolf's first JAVA program";
+char msg = 'S';
+System.out.println(fir);
+System.out.println(msg);
+	}
+
+}
