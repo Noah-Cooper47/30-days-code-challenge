50f1867e89befa0de8af48ad8ea33a3025f94bba
M.Sarathi Ganesh <100061035+Noah-Cooper47@users.noreply.github.com>
2022-06-18 23:16:13 +0530

    Add files via upload

---
+++ Day10.java
@@ -0,0 +1,18 @@
+package Day10;
+
+public class Day10 {
+
+	public static void main(String[] args) {
+		//I am gonna call functions or methods into this call which are declared outside...
+		dARK("dARK_Wolf", 2);
+        System.out.println(Wolf(""));         
+	}
+public static void dARK(String x, int y) {
+	for (int i = 0; i<3; i++) {
+	System.out.println(x); 
+	}
+}
+public static String Wolf(String y) {
+	return y = "47";
+}
+}
